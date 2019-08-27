270aa1d042614413f0ad896f5716419c06b91b33
70360943 <70360943@qq.com>
2019-08-27 09:54:43 +0800

    第一次

--- src/main/java/org/java/HrmsApplication.java
+++ src/main/java/org/java/HrmsApplication.java
@@ -1,7 +1,9 @@
 package org.java;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, MultipartAutoConfiguration.class}) //配出activiti的安全检测
 public class HrmsApplication {
